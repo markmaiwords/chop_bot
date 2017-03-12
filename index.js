93474182c188399cc8cc0f3d1aeba4c0e9b9b04f
markmaiwords <mark.vu.mai@gmail.com>
2017-03-12 18:16:25 -0400

    troubleshooting echo

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 var express = require("express");
 var request = require("request");
 var bodyParser = require("body-parser");
-var server = require('http').Server(app);
 var app = express();
 
-var port = process.env.PORT || 5000;
+app.set('port', (process.env.PORT || 5000))
 
     server.listen(port, function () {
         console.log('Updated : Server listening at port %d', port);
