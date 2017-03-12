f94a7f31ca9f08a410a9d19148fde740cb36ba29
markmaiwords <mark.vu.mai@gmail.com>
2017-03-12 18:09:42 -0400

    changed port code

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ var request = require("request");
 var bodyParser = require("body-parser");
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+var port = process.env.PORT || 5000;
+
+    server.listen(port, function () {
+        console.log('Updated : Server listening at port %d', port);
+    });
 
 // Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}));
