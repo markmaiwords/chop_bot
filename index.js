8c2138cf58752b136940d77694cc61bbd0a215b9
markmaiwords <mark.vu.mai@gmail.com>
2017-03-12 14:08:09 -0400

    new tutorial code

--- index.js
+++ index.js
@@ -23,3 +23,39 @@ app.get("/webhook", function (req, res) {
     res.sendStatus(403);
   }
 });
+
+app.post('/webhook/', function (req, res) {
+    messaging_events = req.body.entry[0].messaging
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i]
+        sender = event.sender.id
+        if (event.message && event.message.text) {
+            text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+})
+
+var token = "EAAO4ltKkADUBADk5zNhzPV8QZBVezODFaQRDW8q5BN4IHFihkN8y4XAKvzqLRZCTrMfase7O2Wwx6okIZBXZBElkpia4VXZBZALKMXf8uttokpkjV1AwMazS6JgbcgJWP5SQcucytdDVODdzdTRo2U0ZBlcFxoj3DIZD"
+
+function sendTextMessage(sender, text) {
+    messageData = {
+        text:text
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
